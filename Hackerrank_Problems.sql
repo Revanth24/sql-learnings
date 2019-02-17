ae7d358e82bf06802bfeb7a6fa370dee7ebd6ad4
Revanth24 <revanthkarunakaran@gmail.com>
2019-02-17 20:29:23 +0530

    Update Hackerrank_Problems.sql

--- Hackerrank_Problems.sql
+++ Hackerrank_Problems.sql
@@ -85,5 +85,58 @@ select name from employee order by name;
 
 select name from employee where salary > 2000 and months < 10 order by employee_id;
 
+/* Query the total population of all cities in CITY where District is California. */
 
+select sum(population) from city where district = 'California';
 
+/* Query the average population of all cities in CITY where District is California. */
+
+select avg(population) from city where district = 'california';
+
+/* Write a query calculating the amount of error (i.e.:  average monthly salaries), and round it up to the next integer. */
+
+select  ceil(avg(salary) - avg(replace(salary, '0', ''))) from employees;
+
+ /* query to find the maximum total earnings for all employees as well as the total number of employees who have maximum 
+    total earnings */
+                                       
+select (salary * months) as income, count(*) from employee group by 1 order by income desc limit 1;
+
+                                       
+ /* Query the following two values from the STATION table:
+
+The sum of all values in LAT_N rounded to a scale of 2 decimal places.
+The sum of all values in LONG_W rounded to a scale of 2 decimal places.*/
+
+ select round(sum(lat_n), 2), round(sum(long_w), 2) from station;                                 
+                                       
+ /* Query the difference between the maximum and minimum populations in CITY. */
+                                       
+ select max(population) - min(population) from city;
+ 
+ /* Query the sum of the populations for all Japanese cities in CITY. The COUNTRYCODE for Japan is JPN. */                                     
+                                       
+select sum(population) from city where countrycode = 'JPN';
+
+/* Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than 38.7880 and less than 137.2345. Truncate your answer to 4 decimal places. */
+                                       
+select  round(sum(lat_n), 4) from station where lat_n > 38.7880 and lat_n < 137.2345;                                       
+
+/* Query the Manhattan Distance between points P1 and P2 and round it to a scale of 4 decimal places. */
+                                       
+select round(abs(min(lat_n) - max(lat_n)) + abs(min(long_w) - max(long_w)), 4)  from station;                                      
+
+/* Query the Euclidean Distance between points P1 and P2 and round it to a scale of 4 decimal places. */
+                                       
+select round(
+    sqrt(
+        power(
+            (min(lat_n) - max(lat_n)), 2) + 
+        power(
+            (min(long_w) - max(long_w)), 2)), 4)  from station;                                      
+                                       
+                                       
+                                       
+                                       
+                                       
+                                       
